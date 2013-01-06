02567d5a058f96201f5a39e795c751ebcbdf8c95
Harish Porwal <harish.porwal@gmail.com>
2013-01-06 18:22:33 +0530

    Basic Home Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Myfinance::Application.routes.draw do
   root :to => 'static_pages#home'
+  match '/home', to: 'static_pages#home'
   match '/contact', to: 'static_pages#contact'
   match '/about', to: 'static_pages#about'
   match '/help', to: 'static_pages#help'
